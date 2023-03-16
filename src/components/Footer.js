0a5e01ab926e049a56969d63c2fbd054eefa2aaf
SalamanderSpeaks <matthewallenstudios@gmail.com>
2023-03-16 14:43:22 -0600

    header & footer

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>2023 One Heart Development</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div>
+      <h2>Matthew Salamander Portfolio</h2>
+      <h3>Full Stack Web Developer</h3>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+        <a href="#resume">Resume</a>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
